dfca64be95e485d7b8ed7a2f1959f856a0a7435f
Ayub-Begimkulov <begimkulov963@gmail.com>
2023-03-06 15:27:53 +0500

    update useI18N hook to correctly update component

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## Version 0.0.5
+
+- `useI18n` hook now returns an object instead of the `I18N` instance. The returned values are now bound and will update it's reference whenever the language changes. Also `getLang` was deprecated in favor of the `lang`.
+
+## Version 0.0.4
+
+- Initial release.
--- src/react/hooks.ts
+++ src/react/hooks.ts
@@ -1,9 +1,16 @@
-import { useCallback, useContext, useEffect, useReducer } from "react";
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 import type { I18N } from "../i18n";
 import { I18NContext } from "./context";
 
-export function useI18N<I18NType extends I18N<any>>() {
-  const i18n = useContext(I18NContext) as I18NType;
+function useI18NContext() {
+  const i18n = useContext(I18NContext);
 
   if (!i18n) {
     throw new Error("can not `useI18NContext` outside of the `I18NProvider`");
@@ -12,8 +19,89 @@ export function useI18N<I18NType extends I18N<any>>() {
   return i18n;
 }
 
+interface ReactI18N<I18NType extends I18N<any>> {
+  readonly lang: ReturnType<I18NType["getLang"]>;
+  get: I18NType["get"];
+  /**
+   * use `lang` instead
+   * @deprecated
+   */
+  getLang: I18NType["getLang"];
+  setLang: I18NType["setLang"];
+  subscribe: I18NType["subscribe"];
+}
+
+export function useI18N<I18NType extends I18N<any>>() {
+  const i18n = useI18NContext() as I18NType;
+  const [{ langState, updateCount }, setLangState] = useState(() => ({
+    langState: i18n.getLang(),
+    updateCount: 0,
+  }));
+  const usesLang = useRef(false);
+
+  useEffect(() => {
+    i18n.subscribe((lang) => {
+      // only update the state if
+      // the lang is used
+      if (!usesLang.current) {
+        return;
+      }
+
+      setLangState((state) => ({
+        langState: lang,
+        updateCount: state.updateCount + 1,
+      }));
+    });
+  }, [i18n]);
+
+  const get: typeof i18n.get = useCallback(
+    (key, ...rest) => {
+      usesLang.current = true;
+      return i18n.get(key, ...rest);
+    },
+    // include the `updateCount` into the deps array
+    // so that `get` function changes it's reference whenever
+    // the languages changes or the translations are loaded
+    [i18n, updateCount]
+  );
+
+  const getLang: typeof i18n.getLang = useCallback(
+    () => {
+      usesLang.current = true;
+      return i18n.getLang();
+    },
+    // include the `langState` into the deps array
+    // so that `get` function changes it's reference whenever
+    // the languages changes
+    [i18n, langState]
+  );
+
+  const setLang: typeof i18n.setLang = useCallback(
+    (newLang) => i18n.setLang(newLang),
+    [i18n]
+  );
+
+  const subscribe: typeof i18n.subscribe = useCallback(
+    (cb, options) => i18n.subscribe(cb, options),
+    [i18n]
+  );
+
+  const reactI18N = {
+    get lang() {
+      usesLang.current = true;
+      return langState;
+    },
+    get,
+    getLang,
+    setLang,
+    subscribe,
+  } as ReactI18N<I18NType>;
+
+  return reactI18N;
+}
+
 export function useTranslate<I18NType extends I18N<any>>() {
-  const i18n = useI18N();
+  const i18n = useI18NContext();
   const [updateCount, triggerUpdate] = useReducer((v) => v + 1, 0);
 
   useEffect(() => {
